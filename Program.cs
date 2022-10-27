25a91fbb03c8f1916d797d9fd74ea35daa3f7032
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-27 20:59:52 +0530

    exact winning position 100 or not

--- Program.cs
+++ Program.cs
@@ -9,11 +9,12 @@
                    SNAKE_BITE = 3;
             int position = 0;
 
-            while (position <= 100)
+            while (position < 100)
             {
                 Random random = new Random();
-                int dice = random.Next(1, 7);  // to generate randomly 1-6 dice roll
-                int checkOption = random.Next(1, 4); //  to generate randomly options
+                int dice = random.Next(1, 7),
+                   checkOption = random.Next(1, 4),
+                   previousPosition = position;
                 switch (checkOption)
                 {
                     case NO_PLAY:
@@ -25,21 +26,21 @@
                     case SNAKE_BITE:
                         position = position - dice;
                         break;
+
                     default:
                         position = 0;
                         break;
                 }
                 Console.WriteLine("postion of player is :" + position);
 
-
-
-                if (position <= 0)
+                if (position > 100)
                 {
-                    Console.WriteLine("Player restarts from 0");
+                    position = previousPosition;
+                    Console.WriteLine("PLAYER STAYS IN SAME POSITION: " + position);
                 }
                 else
                 {
-                    Console.WriteLine("Player stays in same postion");
+                    position = 100;
                 }
                 break;
             }
