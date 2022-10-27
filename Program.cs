156763b66fc558a58e994b25c71278c752c7e88e
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-27 20:54:37 +0530

    checking for options-noplay,ladder,snakebite

--- Program.cs
+++ Program.cs
@@ -4,15 +4,30 @@
     {
         public static void Main(string[]args)
         {
-            //int Position = 0;
-            //Console.WriteLine("Player is at Starting Position: " + Position);
+            const int NO_PLAY = 1,
+          LADDER = 2,
+          SNAKE_BITE = 3;
+            int position = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7),       //to generate 1-6 randomly by rolling dice
+                checkOption = random.Next(1, 4);//to generate random option from NO_PLAY, LADDER, SNAKE_BITE 
 
-            int player1 = 0;
-
-            Random random = new Random(); //to generate random numbers
-
-            player1 = random.Next(1, 7); //to print randomly 1-6
-            Console.WriteLine(player1);
+            switch (checkOption)
+            {
+                case NO_PLAY:
+                    position = 0;
+                    break;
+                case LADDER:
+                    position = position + dice;
+                    break;
+                case SNAKE_BITE:
+                    position = position - dice;
+                    break;
+                default:
+                    position = 0;
+                    break;
+            }
+            Console.WriteLine("postion of player is :" + position);
 
         }
     }
