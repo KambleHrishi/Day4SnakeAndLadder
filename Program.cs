14c04e5c6af62fbb0e004fe0d247c4992cb1d7d9
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-27 20:56:59 +0530

    winning position

--- Program.cs
+++ Program.cs
@@ -5,29 +5,44 @@
         public static void Main(string[]args)
         {
             const int NO_PLAY = 1,
-          LADDER = 2,
-          SNAKE_BITE = 3;
+                       LADDER = 2,
+                   SNAKE_BITE = 3;
             int position = 0;
-            Random random = new Random();
-            int dice = random.Next(1, 7),       //to generate 1-6 randomly by rolling dice
-                checkOption = random.Next(1, 4);//to generate random option from NO_PLAY, LADDER, SNAKE_BITE 
 
-            switch (checkOption)
+            while (position <= 100)
             {
-                case NO_PLAY:
-                    position = 0;
-                    break;
-                case LADDER:
-                    position = position + dice;
-                    break;
-                case SNAKE_BITE:
-                    position = position - dice;
-                    break;
-                default:
-                    position = 0;
-                    break;
+                Random random = new Random();
+                int dice = random.Next(1, 7);  // to generate randomly 1-6 dice roll
+                int checkOption = random.Next(1, 4); //  to generate randomly options
+                switch (checkOption)
+                {
+                    case NO_PLAY:
+                        position = 0;
+                        break;
+                    case LADDER:
+                        position = position + dice;
+                        break;
+                    case SNAKE_BITE:
+                        position = position - dice;
+                        break;
+                    default:
+                        position = 0;
+                        break;
+                }
+                Console.WriteLine("postion of player is :" + position);
+
+
+
+                if (position <= 0)
+                {
+                    Console.WriteLine("Player restarts from 0");
+                }
+                else
+                {
+                    Console.WriteLine("Player stays in same postion");
+                }
+                break;
             }
-            Console.WriteLine("postion of player is :" + position);
 
         }
     }
