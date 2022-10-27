be5b04015adfc712b88f2eb0b140733915d1edb1
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-27 20:52:08 +0530

    rolling a Die

--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@
     {
         public static void Main(string[]args)
         {
-            int Position = 0;
-            Console.WriteLine("Player is at Starting Position: " + Position);
+            //int Position = 0;
+            //Console.WriteLine("Player is at Starting Position: " + Position);
+
+            int player1 = 0;
+
+            Random random = new Random(); //to generate random numbers
+
+            player1 = random.Next(1, 7); //to print randomly 1-6
+            Console.WriteLine(player1);
 
         }
     }
