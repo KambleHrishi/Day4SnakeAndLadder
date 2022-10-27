8e95484c4939cc28dad1247e180e2722f0c60e72
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-27 20:47:01 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day4SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Problem");
+        }
+    }
+}
